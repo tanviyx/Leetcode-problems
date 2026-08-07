2646e36f93443e4c83abe7cfbd6ce1c6398f2592
tanvi yadav <135840245+tanviyx@users.noreply.github.com>
2026-08-07 17:55:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (95.71%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,24 +1,22 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        x=n+m
 
-        def shift(pos, value):
-            # Shift elements one step to the right
-            for k in range(m, pos, -1):
-                nums1[k] = nums1[k - 1]
-            nums1[pos] = value
-
-        for x in nums2:
-            inserted = False
-
-            # Find where to insert x
-            for j in range(m):
-                if x < nums1[j]:
-                    shift(j, x)
-                    m += 1
-                    inserted = True
+        for i in nums2[::-1]:
+            inserted= False
+            while not inserted:
+                if m!=0 and nums1[m-1]<i:
+                    nums1[x-1]=i
+                    inserted=True
+                    x=x-1
                     break
-
-            # If x is larger than every element
-            if not inserted:
-                nums1[m] = x
-                m += 1
+                elif m!=0 and nums1[m-1]>i:
+                    nums1[x-1]=nums1[m-1]
+                    inserted=False
+                    x=x-1
+                else:
+                    nums1[x-1]=i
+                    inserted=True
+                    x=x-1
+                    break
+                m=m-1
